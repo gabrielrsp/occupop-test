916c270becc2966a90627874a2a06db23684dc4d
Gabriel Pires <gabriel.pires@a3data.com.br>
2024-02-12 16:21:56 -0300

    add form components

--- src/pages/ForgotPassword/index.tsx
+++ src/pages/ForgotPassword/index.tsx
@@ -9,15 +9,11 @@ export default function ForgotPassword () {
   return (
     <>
       <div className='forgot-container'>
-
         <div className='login-container'>
-
           <div className='login-container-header'>
             <a href='https://www.lipsum.com/feed/html' >Need help?</a>
           </div>
-
           <div className='content'>
-
             <div className='left-side-wrapper' >
               <div className='logo-container'>
                 <img src={Logo} width={40} alt='logo' />
@@ -27,41 +23,27 @@ export default function ForgotPassword () {
                 <p className="description">Indias's first waterless car cleaning company</p>
               </div>
             </div>
-
-
             <div className='forgot-form'>
               <Header textAlign='center'>
                 <h3> Forgot Password</h3>
               </Header>
-
-
               <Header as="h5" textAlign='center'
               >
-
                 Please enter the email address you'd like your password reset information sent to
-
               </Header>
-
-
               <Form >
                 <FormField>
                   <label>Email</label>
                   <input type='email' placeholder='joe@email.com' />
                 </FormField>
-
                 <button type='submit' >
                   Request reset link
                 </button>
-
                 <Link to={'/'} >Back to login</Link>
-
               </Form>
             </div>
           </div>
-
         </div>
-
-
       </div >
     </>
   );
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './styles.css'
-import { Button, Form, FormField, Header, Input, Label, } from 'semantic-ui-react'
+import { Form, FormField, Header } from 'semantic-ui-react'
 import Logo from '../../images/logo.png';
 import { Link } from 'react-router-dom';
 
@@ -8,33 +8,24 @@ export default function Login () {
   return (
     <>
       <div className='container'>
-
-
-
         <div className='login-container'>
-
           <div className='login-container-header'>
             <a href='https://www.lipsum.com/feed/html' >Need help?</a>
           </div>
-
           <div className='content'>
-
             <div className='left-side-wrapper' >
               <div className='logo-container'>
-                <img src={Logo} width={40} />
+                <img src={Logo} width={40} alt="logo" />
                 <Header as="strong" size={'medium'} >
                   CleanMyCar
                 </Header>
                 <p className="description">Indias's first waterless car cleaning company</p>
               </div>
             </div>
-
-
             <div className='form'>
               <Header textAlign='center' >
                 <h3> Log in</h3>
               </Header>
-
               <Form >
                 <FormField>
                   <label>Email</label>
@@ -44,21 +35,14 @@ export default function Login () {
                   <label>Password</label>
                   <input type='password' placeholder='Enter your Password' />
                 </FormField>
-
-
                 <button type='submit' >
                   Login
                 </button>
-
                 <Link to={'/forgot-password'} >Forgot password?</Link>
-
               </Form>
             </div>
           </div>
-
         </div>
-
-
       </div >
     </>
   );
